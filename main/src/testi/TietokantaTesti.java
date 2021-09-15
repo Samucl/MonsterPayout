d4388c4bd4fbde89821491db0cd5c372c8033dda
eljash <eljas.hirvela@metropolia.fi>
2021-09-15 12:11:20 +0300

    TietokantaTesti luokassa nyt suoritetaan SQL toiminnot: 1. Lisätään uusi rivi Kayttaja tauluun tietokannassa 2. Haetaan Kayttaja taulusta kaikki käyttäjät käyttäjänimellä "Testikäyttäjä" 3. Poistetaan taulusta kaikki rivit joissa Kayttajanimi on "Testikäyttäjä"

--- main/src/testi/TietokantaTesti.java
+++ main/src/testi/TietokantaTesti.java
@@ -18,6 +18,36 @@ public class TietokantaTesti {
 		try {
 			con = DriverManager.getConnection(
 					URL + "?user=" + USERNAME + "&password=" + PASSWORD);
+			
+			Statement stmt = con.createStatement();
+			
+			//SQL syöttö kutsu, tehdään Kayttaja tauluun uusi rivi
+			String query = "INSERT INTO Kayttaja (Kayttajanimi, Salasana, Tilinumero, Sahkoposti, Firstname, Lastname) "
+					+ "values ('Testikäyttäjä', 123, 'FI20 40 8950 1253 1250 20', 'testi@testi.fi', 'Mikko', 'Suomalainen')";
+			
+			stmt.executeQuery(query);
+			
+			//Tehdään SQL haku kutsu ja haetaan Testikäyttäjä/käyttäjät
+			query = "SELECT KayttajaID, Kayttajanimi, Sahkoposti FROM Kayttaja WHERE Kayttajanimi = 'Testikäyttäjä'";
+			
+			ResultSet rs = stmt.executeQuery(query);
+			
+			int luku = 0;
+			while(rs.next()) {
+				System.out.println("Tulos "+luku+" | "
+			+"KayttajaID "+rs.getInt("KayttajaID")
+			+"    Kayttajanimi "+rs.getString("Kayttajanimi")
+			+"    Sahkoposti "+rs.getString("Sahkoposti"));
+			}
+			
+			//Poistetaan Kayttaja taulusta mahdolliset "testikäyttäjät"
+			query = "DELETE FROM Kayttaja WHERE Kayttajanimi = 'Testikäyttäjä'";
+			
+			stmt.execute(query);
+			
+			System.out.println("Testikäyttäjä(t) poistettiin tietokannasta.");
+			
+			
 		} catch (SQLException e) {
 			do {
 				System.err.println("Viesti: "+e.getMessage());
