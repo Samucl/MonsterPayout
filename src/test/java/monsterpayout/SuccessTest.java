40501a0487c3e175b78e98b2f6a6db1ac4a49848
eljashirvela <eljashirvela@172.20.10.6>
2021-10-11 12:20:24 +0300

    Yksi jUnit testi, joka aina menee läpi.

---
+++ src/test/java/monsterpayout/SuccessTest.java
@@ -0,0 +1,15 @@
+package monsterpayout;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SuccessTest {
+
+	@Test                                               
+    @DisplayName("This test is always succesfull")   
+    void successTest() {
+        assertTrue(true);  
+    }
+}
