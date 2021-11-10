7df2b6139da5099032010db2ad8a55faf828cef4
eljash <eljas.hirvela@metropolia.fi>
2021-11-10 15:52:37 +0200

    Uuden slottipelin pohja lähes valmis. Monimutkaiset voittorivit toimivat. Vielä parantelua ja grafiikka.

---
+++ src/main/java/slotgames/PeliTestaaja.java
@@ -0,0 +1,12 @@
+package slotgames;
+
+public class PeliTestaaja {
+
+	public static void main(String[] args) {
+		SlotsMadness peli = new SlotsMadness();
+		peli.spin();
+		peli.checkLines();
+
+	}
+
+}
---
+++ src/main/java/slotgames/SlotSymbol.java
@@ -0,0 +1,113 @@
+package slotgames;
+
+public class SlotSymbol {
+	/*
+	 * Kaikki pelit, jotka käyttävät näitä symbooleja 
+	 * sisältävät enintään 5 vaaka riviä
+	 * (myöhemmin ehkä muokataan joustavammaksi)
+	 */
+	Double[] multipliers = new Double[5];
+	
+	private String name;
+	private boolean isWild = false;
+	private boolean isBonus = false;
+	private boolean isScatter = false;
+	
+	/*
+	 * Mitä isompi luku on probabilityssä, sitä suuremmalla todennäköisyydellä 
+	 * symbooli tulee
+	 */
+	private int probability = 0;
+	
+	/*
+	 * Luotaessa symboolia, annetaan numero
+	 * joka kertoo luokalle millaiset kertoimet
+	 * asetetaan,
+	 * merkkijono joka antaa nimen,
+	 * booleanit jotka määrittävät onki symbooli villi tai bonus
+	 */
+	public SlotSymbol(int type, String name, boolean wild, boolean bonus, boolean scatter) {
+		isWild = wild;
+		isBonus = bonus;
+		isScatter = scatter;
+		this.name = name;
+		setMultipliers(type);
+	}
+	
+	/*
+	 * Perus symboolin constructor
+	 */
+	public SlotSymbol(int type, String name) {
+		isWild = false;
+		isBonus = false;
+		isScatter = false;
+		this.name = name;
+		setMultipliers(type);
+	}
+	
+	private void setMultipliers(int type) {
+		/*
+		 * default tekee symboolista huonoimman "perus" symboolin
+		 * 0 - on kovin symbooli ja niin edelleen
+		 */
+			switch(type) {
+			case 0:
+				multipliers = new Double[]{0.0, 0.5, 3.0, 15.0, 100.0};
+				probability = 2;
+				break;
+			case 1:
+				multipliers = new Double[]{0.0, 0.0, 3.0, 10.0, 60.0};
+				probability = 3;
+				break;
+			case 2:
+				multipliers = new Double[]{0.0, 0.0, 2.0, 6.0, 20.0};
+				probability = 5;
+				break;
+			case 3:
+				multipliers = new Double[]{0.0, 0.0, 1.0, 3.0, 12.0};
+				probability = 10;
+				break;
+			case 4:
+				multipliers = new Double[]{0.0, 0.0, 1.0, 3.0, 8.0};
+				probability = 15;
+				break;
+			case 5:
+				multipliers = new Double[]{0.0, 0.0, 0.5, 2.0, 4.0};
+				probability = 20;
+				break;
+			default:
+				multipliers = new Double[]{0.0, 0.0, 0.5, 2.0, 4.0};
+				probability = 20;
+				break;
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isBonus() {
+		return isBonus;
+	}
+	
+	public boolean isWild() {
+		return isWild;
+	}
+	
+	public boolean isScatter() {
+		return isScatter;
+	}
+	
+	public Double[] getMultipliers() {
+		return multipliers;
+	}
+	
+	public int getProbability() {
+		return probability;
+	}
+	
+	public double getMaxMultiplier() {
+		return multipliers[multipliers.length-1];
+	}
+
+}
